002cb9cbfbe0a49ddbcaf04d735eb4083d43b87b
Mikołaj Sosiński <164782094+mik0lajek@users.noreply.github.com>
2025-05-27 19:45:48 +0200

    Added unit test for registerUser

--- backend/src/test/java/com/fullstackproject/backend/controller/UserControllerTest.java
+++ backend/src/test/java/com/fullstackproject/backend/controller/UserControllerTest.java
@@ -1,13 +1,60 @@
 package com.fullstackproject.backend.controller;
 
+import com.fullstackproject.backend.model.User;
+import com.fullstackproject.backend.security.JwtUtil;
+import com.fullstackproject.backend.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @InjectMocks
+    private UserController userController;
+
     @Test
-    void registerUser() {
+    void registerUser_should_register_new_user() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setPassword("Test_password123");
+
+        when(userService.findByEmail("test@example.com"))
+                .thenReturn(Optional.empty());
+        ResponseEntity<String> response = userController.registerUser(user);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals("User registered successfully", response.getBody());
+    }
+
+    @Test
+    void registerUser_should_not_register_existing_user() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setPassword("Test_password123");
+
+        when(userService.findByEmail("test@example.com"))
+                .thenReturn(Optional.of(user));
+
+        ResponseEntity<String> response = userController.registerUser(user);
+
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+        assertEquals("Username already exists", response.getBody());
     }
 
     @Test
