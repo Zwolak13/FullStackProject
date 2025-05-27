2f2655fb09b81ddb1767652ffa99f7ae1de6d736
Mikołaj Sosiński <164782094+mik0lajek@users.noreply.github.com>
2025-05-27 18:46:00 +0200

    Added UserControllerTest template

---
+++ backend/src/test/java/com/fullstackproject/backend/controller/UserControllerTest.java
@@ -0,0 +1,28 @@
+package com.fullstackproject.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void getUserByUsername() {
+    }
+
+    @Test
+    void loginUser() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void getCurrentUser() {
+    }
+}
