7875e9efc8c5734961d6dc62278427ef0c9886ab
Mikołaj Sosiński <164782094+mik0lajek@users.noreply.github.com>
2025-05-29 13:51:39 +0200

    Test: UserServiceTest unit tests template

---
+++ backend/src/test/java/com/fullstackproject/backend/service/UserServiceTest.java
@@ -0,0 +1,39 @@
+package com.fullstackproject.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void saveUser_should_return_saved_user() {
+
+    }
+
+    @Test
+    void authenticate_should_authenticate_when_user_is_present_and_passwords_match() {
+
+    }
+
+    @Test
+    void authenticate_should_return_empty_if_password_does_not_match() {
+    }
+
+    @Test
+    void authenticate_should_return_empty_if_user_is_not_found() {
+
+    }
+
+    @Test
+    void getCurrentUser_should_return_current_user() {
+
+    }
+
+    @Test
+    void getCurrentUser_should_throw_exception_if_user_is_not_found() {}
+
+    @Test
+    void findByEmail_should_return_user_with_given_email() {
+    }
+}
