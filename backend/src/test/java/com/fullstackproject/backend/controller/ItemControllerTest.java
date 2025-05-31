7c170130082c945932af467f7ee1cfdc089ed9a9
Mikołaj Sosiński <164782094+mik0lajek@users.noreply.github.com>
2025-05-31 20:28:30 +0200

    Test: create ItemControllerTest template

---
+++ backend/src/test/java/com/fullstackproject/backend/controller/ItemControllerTest.java
@@ -0,0 +1,28 @@
+package com.fullstackproject.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemControllerTest {
+
+    @Test
+    void createItem() {
+    }
+
+    @Test
+    void getAllItems() {
+    }
+
+    @Test
+    void getItemById() {
+    }
+
+    @Test
+    void updateItem() {
+    }
+
+    @Test
+    void deleteItem() {
+    }
+}
