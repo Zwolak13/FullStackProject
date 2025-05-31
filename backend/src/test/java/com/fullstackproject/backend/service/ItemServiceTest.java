ae8a7aeca40fee1a14167c06295e53cd609b09b3
Mikołaj Sosiński <164782094+mik0lajek@users.noreply.github.com>
2025-05-31 18:49:47 +0200

    Test: create ItemServiceTest template

---
+++ backend/src/test/java/com/fullstackproject/backend/service/ItemServiceTest.java
@@ -0,0 +1,32 @@
+package com.fullstackproject.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemServiceTest {
+
+    @Test
+    void saveItem() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findByMaxPrice() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
